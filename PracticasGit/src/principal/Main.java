fc18ac9c95ae509edfbb808f13c4556e13aece09
Jaime Lopez <087.jaime.087@gmail.com>
2026-01-13 20:07:53 +0100

    Merge rama jaime

--- PracticasGit/src/principal/Main.java
+++ PracticasGit/src/principal/Main.java
@@ -1,11 +1,15 @@
 package principal;
-
+import principal.Alumno;
 public class Main {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Listado de alumnos Ciudad Escolar");
 		System.out.println("Alumno 1:");
+		
+		Alumno alumno1 = new Alumno("Juan", "Pérez", "12345678A");
+		System.out.println(alumno1);
+
 		
 	}
 
